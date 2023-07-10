3caa1b385b58d836aed6eb5b34c6eb4e0d5cd380
nader <nhashemi6@gmail.com>
2023-07-10 14:09:12 +0330

    Add project files.

---
+++ Nlog.Windows/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Nlog.Windows.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nlog.Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnError_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Extens.LogError(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Nlog.Windows/Util/Extens.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Nlog.Windows.Util
+{
+    public static class Extens
+    {
+        public static void LogError(string MSG)
+        {
+            var config = new LoggingConfiguration();
+
+            var fileTarget = new FileTarget();
+            config.AddTarget("file", fileTarget);
+
+            fileTarget.FileName = "${basedir}/Log/NLogFile.txt";
+            fileTarget.Layout = "${message}";
+
+            var rule2 = new LoggingRule("*", LogLevel.Debug, fileTarget);
+            config.LoggingRules.Add(rule2);
+
+            LogManager.Configuration = config;
+
+            var logger = LogManager.GetLogger("Example");
+
+            logger.Error(MSG);
+        }
+    }
+}
